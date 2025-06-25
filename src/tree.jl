17abe51170938f3eee69b13fca4252c1b53384e8
Daniel Arnström <daniel.arnstrom@liu.se>
2025-06-25 23:58:15 +0200

    Cleanup code for generating binary search tree

--- src/tree.jl
+++ src/tree.jl
@@ -116,18 +116,79 @@ function classify_regions(CRs,hps, reg2hp, ws; reg_ids = nothing, hp_ids = nothi
     return nregs,pregs
 end
 
+function reduce_candidates(criteria,splits,ids)
+    vals = [criteria(s) for s in splits]
+    min_val = minimum(vals)
+    min_ids = findall(==(min_val),vals)
+    return min_val,splits[min_ids],ids[min_ids]
+end
+
+function get_split(CRs,hps,reg2hp,reg_ids,pregs,nregs,branches,criterions,ws)
+
+    hp_ids = reduce(∪,Set(first.(reg2hp[i])) for i in findall(reg_ids));
+    hp_ids = collect(setdiff!(hp_ids,first(b) for b in branches))
+
+    # First use heuristic to find candidates
+    splits = [(reg_ids .* nregs[i], reg_ids .* pregs[i]) for i in hp_ids]
+    min_val,splits,hp_ids = reduce_candidates(criterions[1],splits,hp_ids)
+    if length(branches) > 0 && min_val > 1# Compute the actual split
+        splits = tuple.(classify_regions(CRs,hps,reg2hp,ws;reg_ids,hp_ids,branches)...)
+        for c in criterions
+            min_val,splits,hp_ids = reduce_candidates(c,splits,hp_ids)
+        end
+    end
+    hp_id = hp_ids[1]
+    new_nregs, new_pregs = splits[1]
+    return hp_ids[1], splits[1]
+end
+
+function setup_daqp_workspace(n_theta, nc_max = 250)
+    p=DAQPBase.setup_c_workspace(n_theta);
+    A = Array{Float64}(undef,n_theta,nc_max);
+    b = Array{Float64}(undef,nc_max);
+    bl =-1e30*ones(nc_max)
+    sense = zeros(Cint,nc_max)
+
+    DAQPBase.init_c_workspace_ldp(p,A,b,bl,sense;max_radius=n_theta/2)
+    d_work = unsafe_load(Ptr{DAQPBase.Workspace}(p));
+    unsafe_store!(Ptr{Cdouble}(d_work.settings+fieldoffset(DAQPBase.DAQPSettings,14)),1e-9); # sing_tol
+    return (p=p,A=A,b=b,bl=bl,sense=sense)
+end
+
+function remove_redundant_hps(jump_list,hp_list,hps)
+    hp_rows = findall(jump_list.!== 0)
+    new_hp_ids = sort(unique(hp_list[hp_rows]))
+    hp_old2new = Dict(id => k for (k,id) in enumerate(new_hp_ids))
+    new_hps = hps[:,new_hp_ids]
+
+    new_hp_list = copy(hp_list)
+    for i in hp_rows
+        new_hp_list[i] = hp_old2new[new_hp_list[i]]
+    end
+    return new_hps,new_hp_list
+end
+
+function get_duals(CRs,sol)
+    fbs_dual = Matrix{Float64}[]
+    for cr in CRs
+        lam = zeros(size(cr.lam,1),length(sol.problem.norm_factors))
+        lam[:,cr.AS] = cr.lam
+        push!(fbs_dual,lam)
+    end
+    return [denormalize(f,sol.scaling,sol.translation) for f in fbs_dual]
+end
+
 function build_tree(sol::Solution; daqp_settings = nothing, verbose=1, max_reals=1e12,
         dual=false, bfs=true, clipping=false)
     if sol.status != :Solved
         verbose > 0 && @warn "Cannot build binary search tree. Solution status: $(sol.status)"
         return nothing
     end
-    sol.status != :Solved && return nothing
     verbose > 0 && @info "Building binary search tree" 
 
     CRs = clipping ? get_unsaturated(sol.CRs) : sol.CRs
 
-
+    # Get halfplanes and feedbacks
     hps,reg2hp = get_halfplanes(CRs)
     fbs, fb_ids = get_feedbacks(CRs)
 
@@ -139,71 +200,29 @@ function build_tree(sol::Solution; daqp_settings = nothing, verbose=1, max_reals
         return nothing
     end
 
-    # Setup workspace
-    p=DAQP.setup_c_workspace(sol.problem.n_theta);
-    A = Array{Float64}(undef,sol.problem.n_theta,250);
-    b = Array{Float64}(undef,250);
-    bl =-1e30*ones(250)
-    sense = zeros(Cint,250)
-
-    DAQP.init_c_workspace_ldp(p,A,b,bl,sense;max_radius=sol.problem.n_theta/2)
-    d_work = unsafe_load(Ptr{DAQPBase.Workspace}(p));
-    unsafe_store!(Ptr{Cdouble}(d_work.settings+fieldoffset(DAQP.DAQPSettings,14)),1e-9); # sing_tol
-    !isnothing(daqp_settings) && settings(p,daqp_settings)
-    ws = (p=p,A=A,b=b,bl=bl,sense=sense)
+    ws = setup_daqp_workspace(sol.problem.n_theta)
 
     # Do initial classification
-    nh = size(hps,2)
     nregs,pregs = classify_regions(CRs,hps,reg2hp,ws)
-    hp_list, jump_list = Int[0],Int[0]
 
-    nR = length(CRs)
-
-    N0 = (trues(nR),[],1)
-    U = [N0]
     get_fbid = s->Set{Int}(fb_ids[s])
-    split_objective = dual ? x->max(sum.(x)...) : x-> max(length.(get_fbid.(x))...)
-
+    criterions = dual ? [x->max(sum.(x)...)] :  [s->max(length.(get_fbid.(s))...),
+                                                 s->max(sum.(s)...),
+                                                 s->min(length.(get_fbid.(s))...)]
     # Start exploration
+    hp_list, jump_list = Int[0],Int[0]
     depth = 0
     tree_pop! = bfs ? popfirst! : pop!
+    U = [(trues(length(CRs)),[],1)]
     while !isempty(U)
         reg_ids, branches, self_id = tree_pop!(U)
         depth = max(depth,length(branches))
 
-        hp_ids = reduce(∪,Set(first.(reg2hp[i])) for i in findall(reg_ids));
-        hp_ids = collect(setdiff!(hp_ids,first(b) for b in branches))
+        # Get halfplane to cut
+        hp_id, (new_nregs, new_pregs) = get_split(CRs,hps,reg2hp,reg_ids,pregs,nregs,branches,criterions,ws)
+        #vals = [c((new_nregs,new_pregs)) for c in criterions]
 
-        # First use heuristic to find candidates 
-        splits = [(reg_ids .* nregs[i], reg_ids .* pregs[i]) for i in hp_ids]
-        vals = [split_objective(s) for s in splits]
-        min_val = minimum(vals)
-        min_ids = findall(==(min_val),vals)
-        hp_ids = hp_ids[min_ids]
-        if length(branches) > 0 && min_val > 1# Compute the actual split
-            splits = tuple.(classify_regions(CRs,hps,reg2hp,ws;reg_ids,hp_ids,branches)...)
-            vals =[split_objective(s) for s in splits]
-            min_val = minimum(vals)
-            min_ids = findall(==(min_val),vals)
-            # Among the minimum values, do new split to maximum regions that split
-            if(!dual)
-                vals =[max(sum.(s)...) for s in splits[min_ids]]
-                min_val_second,min_id = findmin(vals)
-
-                min_ids = min_ids[findall(==(min_val_second),vals)]
-                vals =[min(length.(get_fbid.(s))...) for s in splits[min_ids]]
-                min_val_third,min_id = findmin(vals)
-            else
-                min_id = 1; # Pick first
-            end
-
-            hp_id = hp_ids[min_ids[min_id]]
-            new_nregs,new_pregs = splits[min_ids[min_id]]
-        else
-            hp_id = hp_ids[1] 
-            new_nregs, new_pregs = splits[min_ids[1]]
-        end
-
+        # Update tree for current node
         next_id = length(hp_list)+1
         hp_list[self_id] = hp_id
         jump_list[self_id] = next_id-self_id
@@ -216,7 +235,8 @@ function build_tree(sol::Solution; daqp_settings = nothing, verbose=1, max_reals
         for (new_regs,new_regs_comp,next, hp_sign) in [(new_nregs,new_pregs,next_id,-1), (new_pregs,new_nregs,next_id+1,1)]
             fb_cands = get_fbid(new_regs)
             if length(fb_cands) == 0
-                @debug "Empty region -> Defaulting to leaf node" min_val findall(new_nregs) findall(new_pregs) findall(reg_ids) branches
+                @debug "Empty region -> Defaulting to leaf node"
+                @debug "" min_val findall(new_nregs) findall(new_pregs) findall(reg_ids) branches
                 jump_list[next] = 0 # pointing at root node -> leaf
                 # Try to pick region that "disappeared", otherwise pick first region in parent
                 reg_id  = isempty(fb_cands) ? findfirst(reg_ids) : findfirst(reg_ids .* .!new_regs_comp)
@@ -229,36 +249,22 @@ function build_tree(sol::Solution; daqp_settings = nothing, verbose=1, max_reals
             end
         end
     end
-    # Remove superfluous HPs
-    hp_rows = findall(jump_list.!== 0)
-    new_hp_ids = sort(unique(hp_list[hp_rows]))
-    hp_old2new = Dict(id => k for (k,id) in enumerate(new_hp_ids))
-    new_hps = hps[:,new_hp_ids]
 
-    new_hp_list = copy(hp_list)
-    for i in hp_rows
-        new_hp_list[i] = hp_old2new[new_hp_list[i]]
-    end
+    # Remove superfluous HPs
+    hps,hp_list= remove_redundant_hps(jump_list,hp_list,hps)
 
     # Denormalize 
-    hps = denormalize(new_hps,sol.scaling,sol.translation;hps=true)
+    hps = denormalize(hps,sol.scaling,sol.translation;hps=true)
     fbs = [denormalize(f,sol.scaling,sol.translation) for f in fbs]
 
-    fbs_dual = Matrix{Float64}[]
-    if dual # XXX would prefer sparse representation, but makes C-code a lot messier...
-        for cr in CRs
-            lam = zeros(size(cr.lam,1),length(sol.problem.norm_factors))
-            lam[:,cr.AS] = cr.lam
-            push!(fbs_dual,lam)
-        end
-        fbs_dual = [denormalize(f,sol.scaling,sol.translation) for f in fbs_dual]
-    end
+    # Extract dual variables
+    fbs_dual = dual ?  get_duals(CRs,sol) : Matrix{Float64}[] # XXX sparse representation makes C-code messy...
 
     # Cleanup
     DAQP.free_c_workspace(ws.p)
 
     zlims = clipping ? sol.problem.out_lims : zeros(0,2)
-    return BinarySearchTree(hps,fbs,new_hp_list,jump_list,depth, fbs_dual, zlims)
+    return BinarySearchTree(hps,fbs,hp_list,jump_list,depth, fbs_dual, zlims)
 end
 
 function evaluate(bst::BinarySearchTree,θ)
